a2a85e8f7558f026a68f0f7f6771d2fd63b7af67
w1121 <w1121@yandex.ru>
2014-12-13 05:08:58 +0400

    ActiveREPAIR: критериии при бое EnemyNotFound: добавлен ремонт кораблей Добавлена новая точка - центр - FindStartPoint. Изменена Траектория движения - FindHeadingTarget Добавлен флаг в main для определения движения в центральной точке - используется в FindHeadingTarget

--- Development/BattleShips/Battleships/target/BattleShips.hs
+++ Development/BattleShips/Battleships/target/BattleShips.hs
@@ -5,77 +5,77 @@
 #endif
 
 /*--------------------------- Signal numbers ----------------------------*/
-#ifndef sig_BasicTypes_InitDataComplete
-#define sig_BasicTypes_InitDataComplete XHS_START+1
+#ifndef InitDataComplete
+#define InitDataComplete XHS_START+1
 #endif
-#ifndef sig_BasicTypes_StartSimulation
-#define sig_BasicTypes_StartSimulation XHS_START+2
+#ifndef StartSimulation
+#define StartSimulation XHS_START+2
 #endif
-#ifndef sig_BasicTypes_FinishSimulation
-#define sig_BasicTypes_FinishSimulation XHS_START+3
+#ifndef FinishSimulation
+#define FinishSimulation XHS_START+3
 #endif
-#ifndef sig_BasicTypes_OrderSpeedRequest
-#define sig_BasicTypes_OrderSpeedRequest XHS_START+4
+#ifndef OrderSpeedRequest
+#define OrderSpeedRequest XHS_START+4
 #endif
-#ifndef sig_BasicTypes_OrderTurnRequest
-#define sig_BasicTypes_OrderTurnRequest XHS_START+5
+#ifndef OrderTurnRequest
+#define OrderTurnRequest XHS_START+5
 #endif
-#ifndef sig_BasicTypes_OrderFireRequest
-#define sig_BasicTypes_OrderFireRequest XHS_START+6
+#ifndef OrderFireRequest
+#define OrderFireRequest XHS_START+6
 #endif
-#ifndef sig_BasicTypes_DestroyShip
-#define sig_BasicTypes_DestroyShip XHS_START+7
+#ifndef DestroyShip
+#define DestroyShip XHS_START+7
 #endif
-#ifndef sig_BasicTypes_StrategyVictory
-#define sig_BasicTypes_StrategyVictory XHS_START+8
+#ifndef StrategyVictory
+#define StrategyVictory XHS_START+8
 #endif
-#ifndef sig_BasicTypes_StrategyLose
-#define sig_BasicTypes_StrategyLose XHS_START+9
+#ifndef StrategyLose
+#define StrategyLose XHS_START+9
 #endif
-#ifndef sig_BasicTypes_OrderSpeedResponse
-#define sig_BasicTypes_OrderSpeedResponse XHS_START+10
+#ifndef OrderSpeedResponse
+#define OrderSpeedResponse XHS_START+10
 #endif
-#ifndef sig_BasicTypes_OrderSpeedReject
-#define sig_BasicTypes_OrderSpeedReject XHS_START+11
+#ifndef OrderSpeedReject
+#define OrderSpeedReject XHS_START+11
 #endif
-#ifndef sig_BasicTypes_OrderTurnResponse
-#define sig_BasicTypes_OrderTurnResponse XHS_START+12
+#ifndef OrderTurnResponse
+#define OrderTurnResponse XHS_START+12
 #endif
-#ifndef sig_BasicTypes_OrderTurnReject
-#define sig_BasicTypes_OrderTurnReject XHS_START+13
+#ifndef OrderTurnReject
+#define OrderTurnReject XHS_START+13
 #endif
-#ifndef sig_BasicTypes_OrderFireResponse
-#define sig_BasicTypes_OrderFireResponse XHS_START+14
+#ifndef OrderFireResponse
+#define OrderFireResponse XHS_START+14
 #endif
-#ifndef sig_BasicTypes_OrderFireReject
-#define sig_BasicTypes_OrderFireReject XHS_START+15
+#ifndef OrderFireReject
+#define OrderFireReject XHS_START+15
 #endif
-#ifndef sig_BasicTypes_ShipsCurrentState
-#define sig_BasicTypes_ShipsCurrentState XHS_START+16
+#ifndef ShipsCurrentState
+#define ShipsCurrentState XHS_START+16
 #endif
-#ifndef sig_BasicTypes_VisibleShip
-#define sig_BasicTypes_VisibleShip XHS_START+17
+#ifndef VisibleShip
+#define VisibleShip XHS_START+17
 #endif
-#ifndef sig_BasicTypes_messageLog
-#define sig_BasicTypes_messageLog XHS_START+18
+#ifndef messageLog
+#define messageLog XHS_START+18
 #endif
-#ifndef sig_BasicTypes_messageError
-#define sig_BasicTypes_messageError XHS_START+19
+#ifndef messageError
+#define messageError XHS_START+19
 #endif
-#ifndef sig_Simulator_SendData
-#define sig_Simulator_SendData XHS_START+20
+#ifndef SendData
+#define SendData XHS_START+20
 #endif
-#ifndef sig_Simulator_InitSocket
-#define sig_Simulator_InitSocket XHS_START+21
+#ifndef InitSocket
+#define InitSocket XHS_START+21
 #endif
-#ifndef sig_Simulator_SendDataComplete
-#define sig_Simulator_SendDataComplete XHS_START+22
+#ifndef SendDataComplete
+#define SendDataComplete XHS_START+22
 #endif
-#ifndef sig_Simulator_InitSocketComplete
-#define sig_Simulator_InitSocketComplete XHS_START+23
+#ifndef InitSocketComplete
+#define InitSocketComplete XHS_START+23
 #endif
-#ifndef sig_Simulator_CloseClientSocket
-#define sig_Simulator_CloseClientSocket XHS_START+24
+#ifndef CloseClientSocket
+#define CloseClientSocket XHS_START+24
 #endif
 
 /*---------------------------- Process data -----------------------------*/
